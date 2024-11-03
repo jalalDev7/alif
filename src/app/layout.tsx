51ab676ac01f79000f1444d1caa9c393bbb62c75
Mohamed Jalal <technochtak3@gmail.com>
2024-11-03 21:29:11 +0100

    main

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { Roboto } from "next/font/google";
 
-const roboto = Roboto({ weight: "400" });
+const roboto = Roboto({ weight: "400", subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "ALIF | Bring happines to your pets",
